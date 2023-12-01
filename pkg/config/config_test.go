f44e8c525ac57a99ed0563b4ff026f0f87c50cd4
Braxton Plaxco <bplaxco@redhat.com>
2023-12-01 14:20:58 -0500

    Add more test cases and fix parsing bugs

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -6,8 +6,57 @@ import (
 	"testing"
 )
 
-func TestLoadConfigFromFile(t *testing.T) {
+// TestFullLoadConfigFromFile tests that the config can be completely set
+// by a config file
+func TestFullLoadConfigFromFile(t *testing.T) {
 	config, err := LoadConfigFromFile("../../testdata/full-config.toml")
+
+	if err != nil {
+		t.Errorf("Load returned an error %v", err)
+	}
+
+	if config == nil {
+		t.Error("Got a nil config")
+	}
+
+	// Check values
+	tests := []struct {
+		expected string
+		actual   string
+	}{
+		{
+			expected: "set-by-config",
+			actual:   config.Scanner.Gitleaks.Version,
+		},
+		{
+			expected: "/tmp/leaktk/scanner",
+			actual:   config.Scanner.Workdir,
+		},
+		{
+			expected: "10",
+			actual:   fmt.Sprint(config.Scanner.Patterns.RefreshInterval),
+		},
+		{
+			expected: "https://example.com/leaktk/patterns/main/target",
+			actual:   config.Scanner.Patterns.Server.URL,
+		},
+		{
+			expected: "placeholder_auth_token",
+			actual:   config.Scanner.Patterns.Server.AuthToken,
+		},
+		{
+			expected: "DEBUG",
+			actual:   config.Logger.Level,
+		},
+	}
+
+	for _, test := range tests {
+		assert.Equal(t, test.expected, test.actual)
+	}
+}
+
+func TestPartialLoadConfigFromFile(t *testing.T) {
+	config, err := LoadConfigFromFile("../../testdata/partial-config.toml")
 
 	if err != nil {
 		t.Errorf("Load returned an error %v", err)
@@ -35,7 +84,7 @@ func TestLoadConfigFromFile(t *testing.T) {
 			actual:   fmt.Sprint(config.Scanner.Patterns.RefreshInterval),
 		},
 		{
-			expected: "https://raw.githubusercontent.com/leaktk/patterns/main/target",
+			expected: "https://example.com/leaktk/patterns/main/target",
 			actual:   config.Scanner.Patterns.Server.URL,
 		},
 		{
@@ -50,5 +99,17 @@ func TestLoadConfigFromFile(t *testing.T) {
 
 	for _, test := range tests {
 		assert.Equal(t, test.expected, test.actual)
+	}
+}
+
+func TestLoadConfigFromFileWithInvalidLogLevel(t *testing.T) {
+	config, err := LoadConfigFromFile("../../testdata/invalid-log-level.toml")
+
+	if err == nil {
+		t.Error("An error was not returned")
+	}
+
+	if config != nil {
+		t.Errorf("Config was returned when it shouldn't be: %v", config)
 	}
 }
