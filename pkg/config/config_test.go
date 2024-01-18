12120d3be045a343fcf1b5fa172c7597e5c252fa
Braxton Plaxco <bplaxco@redhat.com>
2024-01-18 13:37:43 -0500

    Add more test cases and fix parsing bugs

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -68,6 +68,53 @@ func TestPartialLoadConfigFromFile(t *testing.T) {
 		actual   string
 	}{
 		{
+			expected: "set-by-config",
+			actual:   config.Scanner.Gitleaks.Version,
+		},
+		{
+			expected: "/tmp/leaktk/scanner",
+			actual:   config.Scanner.Workdir,
+		},
+		{
+			expected: "10",
+			actual:   fmt.Sprint(config.Scanner.Patterns.RefreshInterval),
+		},
+		{
+			expected: "https://example.com/leaktk/patterns/main/target",
+			actual:   config.Scanner.Patterns.Server.URL,
+		},
+		{
+			expected: "placeholder_auth_token",
+			actual:   config.Scanner.Patterns.Server.AuthToken,
+		},
+		{
+			expected: "DEBUG",
+			actual:   config.Logger.Level,
+		},
+	}
+
+	for _, test := range tests {
+		assert.Equal(t, test.expected, test.actual)
+	}
+}
+
+func TestPartialLoadConfigFromFile(t *testing.T) {
+	config, err := LoadConfigFromFile("../../testdata/partial-config.toml")
+
+	if err != nil {
+		t.Errorf("Load returned an error %v", err)
+	}
+
+	if config == nil {
+		t.Error("Got a nil config")
+	}
+
+	// Check values
+	tests := []struct {
+		expected string
+		actual   string
+	}{
+		{
 			expected: "7.6.1",
 			actual:   config.Scanner.Gitleaks.Version,
 		},
@@ -100,7 +147,6 @@ func TestPartialLoadConfigFromFile(t *testing.T) {
 
 func TestLoadConfigFromFileWithInvalidLogLevel(t *testing.T) {
 	config, err := LoadConfigFromFile("../../testdata/invalid-log-level.toml")
-
 	assert.Error(t, err, "Expected error from invalid config")
 	assert.Nil(t, config, "Expected invalid config to be nil")
 }
