f771d1985e5b6ed348c4fb1ec76eb363c33acf98
Joshua Padman <joshua@padman.id.au>
2024-08-30 13:13:14 +1000

    Finished basic implementation
    uploaded fake-leaks git repo inside a container to
    quay.io/wizzy/fake-leaks as in examples folder.

--- pkg/scanner/gitleaks.go
+++ pkg/scanner/gitleaks.go
@@ -139,7 +139,7 @@ func (g *Gitleaks) walkScan(detector *detect.Detector, scanResource resource.Res
 				return nil
 			}
 			if mimetype.MIME.Type == "application" {
-				logger.Error("skipping binary file: path=%q", path)
+				logger.Info("skipping binary file: path=%q", path)
 				return nil // skip binary files
 			}
 
