0abbf2dd002df474fa4962a0b72558e807f522c0
thewizzy <858086+thewizzy@users.noreply.github.com>
2024-09-24 09:36:37 +1000

    Fixed gitlog failing on missing commits & spelling (#49)

--- pkg/scanner/error_test.go
+++ pkg/scanner/error_test.go
@@ -12,11 +12,11 @@ func TestL(t *testing.T) {
 		Message: "test error message",
 	}
 	t.Run("non fatal output", func(t *testing.T) {
-		assert.Equal(t, "error occured, code 2 (ScanError): test error message", e.String())
+		assert.Equal(t, "error occurred, code 2 (ScanError): test error message", e.String())
 	})
 
 	e.Fatal = true
 	t.Run("fatal output", func(t *testing.T) {
-		assert.Equal(t, "fatal error occured, code 2 (ScanError): test error message", e.String())
+		assert.Equal(t, "fatal error occurred, code 2 (ScanError): test error message", e.String())
 	})
 }
