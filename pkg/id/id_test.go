c625aaf4c8ac05657977f41493590d5fd08278e9
Braxton Plaxco <bplaxco@redhat.com>
2025-02-12 16:53:10 -0500

    Shorten IDs
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- pkg/id/id_test.go
+++ pkg/id/id_test.go
@@ -13,16 +13,16 @@ func TestID(t *testing.T) {
 	})
 
 	t.Run("IDsAreSameLength", func(t *testing.T) {
-		assert.Equal(t, 64, len(ID()))
-		assert.Equal(t, 64, len(ID("foo")))
-		assert.Equal(t, 64, len(ID("foo", "bar")))
+		assert.Equal(t, 11, len(ID()))
+		assert.Equal(t, 11, len(ID("foo")))
+		assert.Equal(t, 11, len(ID("foo", "bar")))
 	})
 
 	t.Run("IDsAreHexadecimal", func(t *testing.T) {
 		// Run the test a bunch of times on random and parameterized ID calls
 		for i := 0; i < 100; i++ {
-			assert.Regexp(t, "^[0-9a-f]+$", ID())
-			assert.Regexp(t, "^[0-9a-f]+$", ID(strconv.Itoa(i), ID()))
+			assert.Regexp(t, `^[\w-]+$`, ID())
+			assert.Regexp(t, `^[\w-]+$`, ID(strconv.Itoa(i), ID()))
 		}
 	})
 }
